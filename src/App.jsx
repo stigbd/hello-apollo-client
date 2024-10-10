c28e975c9277c5ce68ed2afaa69dfdae557f77a2
Stig B. Dørmænen <stigbd@gmail.com>
2024-10-10 15:27:29 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+// Import everything needed to use the `useQuery` hook
+import { useQuery, gql } from "@apollo/client";
+
+// Define the query
+const GET_BOOKS = gql`
+  query GetBooks {
+    books {
+      id
+      title
+      author
+    }
+  }
+`;
+
+// Create a component that uses the `useQuery` hook
+function DisplayBooks() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.books.map(({ id, title, author }) => (
+    <div key={id}>
+      <h3>{title}</h3>
+      <b>Author:</b>
+      <p>{author}</p>
+      <br />
+    </div>
+  ));
+}
+
+// Export the component
+export default function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplayBooks />
+    </div>
+  );
+}
